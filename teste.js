93df8163f4b33555ab021156cac937bf3d7b1d7b
Derson <wanderson.sousa@tahto.com.br>
2024-07-21 11:31:38 -0300

    Feat

---
+++ teste.js
@@ -0,0 +1,14 @@
+let fruta = "a"
+
+switch(fruta){
+    case "laranja":
+        console.log('Suco de laranja')
+        break
+    case "banana":
+        console.log('Vitamina de banana')
+        break
+    case "maça":
+        console.log('Suco de maça')
+    default:
+        console.log('Você quer água')
+}
